f00d112aa8a15f709b14c72fac3f94f1792e9f2c
Zachary <poisontusk@gmail.com>
2023-03-21 01:04:23 +0800

    use

--- package/src/lib.rs
+++ package/src/lib.rs
@@ -1,3 +1,5 @@
+fn serve_common_order() {}
+
 mod front_of_house {
 
     pub mod hosting { // 这是一个子 module
@@ -11,6 +13,13 @@ mod front_of_house {
         fn take_payment() {}
     }
 
+    fn serve_order() {
+        // super 找到 front_of_house 的上级模块，调用 serve_common_order
+        super::serve_common_order(); 
+
+    }
+
+
 }
 
 pub fn eat_at_restaurant() {
@@ -22,5 +31,47 @@ pub fn eat_at_restaurant() {
 
     // 使用相对路径调用
     front_of_house::hosting::add_to_waitlist();
+}
 
+mod back_of_house {
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    // 一个 public 的 struct
+    pub struct Breakfast {
+        pub toast: String,      // public
+        seasonal_fruit: String, // private
+    }
+
+    impl Breakfast {
+        // 关联函数
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast { 
+                toast: String::from(toast), 
+                seasonal_fruit: String::from("peaches"),
+             }
+        }
+    }
+}
+
+pub fn eat_up() {
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+    //meal.seasonal_fruit = String::from("bluecerries"); // 这里报错，因为 seasonal_fruit 是私有的
+}
+
+// 引入模块 hosting 
+//use crate::front_of_house::hosting;
+
+// 使用相对路径引入 hosting，因为 front_of_house 在同一级
+use front_of_house::hosting;
+
+pub fn eat_use() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    // hosting::seat_at_table(); 报错 因为这个方法是私有的
 }
--- package/src/main.rs
+++ package/src/main.rs
@@ -1,5 +1,18 @@
+// 引入时直接指定到 HashMap 本身
+use std::collections::HashMap;
+
+use std::fmt::Result;
+use std::io::Result as IOResult;  // 设置本地别名为 IOResult
+
+fn f1() -> Result {
+
+}
+
+fn f2() -> IOResult<i32> {
+
+}
+
 fn main() {
-
-
-    
+    let mut map = HashMap::new();
+    map.insert(1, 2);    
 }
