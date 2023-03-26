7d4f2b18bab111661247b3433f1b9887a52a7fe5
Zachary <poisontusk@gmail.com>
2023-03-27 00:51:44 +0800

    传播错误

--- panic/src/main.rs
+++ panic/src/main.rs
@@ -1,8 +1,11 @@
-use std::error;
+use std::error::{self, Error};
 use std::fs::File;
 use std::io::ErrorKind;
+use std::io;
+use std::io::Read;
 
-fn main() {
+// Box<dyn Error> 是一个 trait 对象
+fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
     // let f = File::open("hello.txt");
 
@@ -42,6 +45,60 @@ fn main() {
 
     //let f = File::open("hello.txt").unwrap();
 
-    let f = File::open("hello.txt").expect("文件不存在");
+    //let f = File::open("hello.txt").expect("文件不存在");
 
+    flood();
+    let f = File::open("hello.txt")?;
+    Ok(())
+}
+
+fn flood() {
+    //let name = read_username_from_file();
+    //let name = read_username_from_file_fast();
+    let name = read_username_from_file_chain();
+
+    match name {
+        Ok(s) => println!("name = {}", s),
+        Err(e) => println!("e is {}", e),
+    };
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    // 尝试打开文件，返回 Result<File, Error> 类型
+    let f = File::open("hello.txt");
+
+    // 对 Result<File, Error> 进行类型匹配
+    let mut f = match f {
+        Ok(file) => file, // 如果返回 File 将其赋值给 f
+        Err(err) => return Err(err), // 否则方法返回 Error
+    };
+
+    let mut s = String::new();
+
+    // 把文件内容读取到 s 字符串
+    // 这个 match 表达式最后没有 ; ，即它是这个函数最后一个表达式，即函数结果
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s), // 如果成功将 s 字符串返回
+        Err(err) => Err(err), // read_to_string 失败返回 err        
+    }
+}
+
+fn read_username_from_file_fast() -> Result<String, io::Error> {
+
+    let mut f = File::open("hello.txt")?;
+    // 加问号等同于与下边 match
+    // let mut f = match f {
+    //     Ok(file) => file, // 如果返回 File 将其赋值给 f
+    //     Err(err) => return Err(err), // 否则方法返回 Error
+    // };
+
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn read_username_from_file_chain() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
 }
