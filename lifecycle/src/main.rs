1a11e4bbc11f0555231e07dc3ddea462803eed81
Zachary <poisontusk@gmail.com>
2023-03-30 00:31:04 +0800

    life cycle

--- lifecycle/src/main.rs
+++ lifecycle/src/main.rs
@@ -8,11 +8,27 @@ fn main() {
     // }
     // println!("{}", r);
 
+    // let str1 = String::from("abc");
+    // {
+    //     // str2 是静态的全程序作用域有效
+    //     let str2 = "xyz";
+    //     let result = large(str1.as_str(), str2);
+    //     println!("{}", result);
+    // }
+
     let str1 = String::from("abc");
-    let str2 = "xyz";
-    
-    let l = large(str1.as_str(), str2);
-    
+    let result;
+    {
+        // str2 是静态的全程序作用域有效
+        let str2 = String::from("xyz");
+        // large 返回生命周期是 str1 与 str2 较短的那个
+        // 所以这里用的是 str2 的生命周期, 赋给了 result 
+        result = large(str1.as_str(), str2.as_str()); // error
+    } // 这里 str2 离开了作用域，但依然在借用的状态
+
+    // str2 必须在外部作用域结束前一直有效才行
+    // result 是 str2 的生命周期，str2 已经释放了 所以有问题
+    println!("{}", result);
 
 }
 
