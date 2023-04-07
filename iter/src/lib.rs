e809ecd0f1bf2e47e6f0434a31f65f9c7f608d94
Zachary <poisontusk@gmail.com>
2023-04-07 13:28:34 +0800

    iter 捕获变量

--- iter/src/lib.rs
+++ iter/src/lib.rs
@@ -1,6 +1,36 @@
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoe_in_my_size(shoes: Vec<Shoe>, size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|shoe| shoe.size == size).collect()
+}
+
+
+
 #[cfg(test)]
 
 mod tests {
+    use crate::{Shoe, shoe_in_my_size};
+
+
+    #[test]
+    fn iter_filter_my_shoes() {
+        let shoes = vec![
+            Shoe { size: 10, style: String::from("sneaker")},
+            Shoe { size: 12, style: String::from("sandal")},
+            Shoe { size: 10, style: String::from("boot")},
+        ];
+
+        let my_shoes = shoe_in_my_size(shoes, 10);
+
+        assert_eq!(my_shoes, vec![
+            Shoe { size: 10, style: String::from("sneaker")},
+            Shoe { size: 10, style: String::from("boot")},
+        ]);
+    }
 
     #[test]
     fn iterator_demonstration() {
@@ -11,6 +41,26 @@ mod tests {
         assert_eq!(v1_iter.next(), Some(&1));
         assert_eq!(v1_iter.next(), Some(&2));
         assert_eq!(v1_iter.next(), Some(&3));
+    }
+
+    #[test]
+    fn iter_sum() {
+        let v1 = vec![1, 2, 3];
+        let v1_iter = v1.iter();
+        let v2 = v1_iter.sum();
+        assert_eq!(6, v2);
+    }
+
+    #[test]
+    fn iter_sum_2() {
+        let v1 = vec![1, 2, 3];
+
+        // v1.iter().map(|x| x + 1) 并不会对每个元素进行加 1，因为没有消耗迭代器
+        // v1.iter().map(|x| x + 1);
+
+        // Vec<_> 由编译器自行推断
+        let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+        assert_eq!(v2, vec![2, 3, 4]);
     }
 
 }
