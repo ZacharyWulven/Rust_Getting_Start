5e23945c9ad193250209d0c010805b9dcfe766fb
Zacharyah <renyi4@vipkid.com.cn>
2025-02-20 11:40:53 +0800

    iter 性能

--- iter/src/main.rs
+++ iter/src/main.rs
@@ -1,3 +1,4 @@
+use std::iter::Iterator;
 use iter::Counter;
 
 fn main() {
@@ -29,4 +30,19 @@ fn one() {
 pub trait Iterator {
     type Item;
     fn next(&mut self) -> Option<Self::Item>;
+}
+
+fn mp3() {
+    let buffer: &mut [i32];
+    let coefficients: [i64; 12];
+    let qlp_shift: i16;
+
+    for i in 12..buffer.len()  {
+        let prediction = coefficients.iter()
+            .zip(&buffer[i - 12..i])
+            .map(|(&c, &s)| c * s as i64)
+            .sum::<i64>() >> qlp_shift;
+        let delta = buffer[i];
+        buffer[i] = prediction as i32 + delta;
+    }
 }
